151b1e97720ab2d747a4b48e03bbeb3ce573e277
Roger Worrell <raworre@gmail.com>
2023-04-07 17:31:06 -0600

    adding hard-coded user to login response

--- apps/user-manager/src/test/java/com/warlock/user/controller/UserControllerTest.java
+++ apps/user-manager/src/test/java/com/warlock/user/controller/UserControllerTest.java
@@ -1,6 +1,7 @@
 package com.warlock.user.controller;
 
 
+import com.warlock.user.model.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,7 +19,19 @@ public class UserControllerTest {
         var response = userController.login();
         assertTrue(response.hasBody());
         assertThat(response.getBody()).isNotNull();
-        assertTrue(response.getBody().containsKey("token"));
-        assertTrue(response.getBody().get("token").contains("CHUMP"));
+        var responseBody = response.getBody();
+        assertTrue(responseBody.containsKey("token"));
+        assertTrue(responseBody.containsKey("user"));
+        var token = (String) responseBody.get("token");
+        assertThat(token).contains("CHUMP");
+        var user = (User) responseBody.get("user");
+        assertThat(user.getUsername()).isEqualTo("hrothgar.warlock");
+        assertThat(user.getId()).isEqualTo("1a");
+        assertThat(user.getPassword()).isEqualTo("p4ssw0rd");
+        var profile = user.getProfile();
+        assertThat(profile.getId()).isEqualTo("2b");
+        assertThat(profile.getBirthdate()).isEqualTo("1981-06-18");
+        assertThat(profile.getFirstName()).isEqualTo("Hrothgar");
+        assertThat(profile.getLastName()).isEqualTo("Warlock");
     }
 }
