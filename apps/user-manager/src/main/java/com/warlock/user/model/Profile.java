82057488cbbed2d5037a97b6d1c2789be32a23b1
Roger Worrell <raworre@gmail.com>
2023-04-07 17:30:39 -0600

    adding basic profile and user objects

---
+++ apps/user-manager/src/main/java/com/warlock/user/model/Profile.java
@@ -0,0 +1,20 @@
+package com.warlock.user.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+@Document("profile")
+@Data
+public class Profile {
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthdate;
+    @DBRef
+    private User user;
+}
