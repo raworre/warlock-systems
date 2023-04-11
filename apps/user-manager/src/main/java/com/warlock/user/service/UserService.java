ef869f517521fce71bc12e4bd5415019411b0be1
Roger Worrell <raworre@gmail.com>
2023-04-10 18:43:10 -0600

    cleaning up imports

--- apps/user-manager/src/main/java/com/warlock/user/service/UserService.java
+++ apps/user-manager/src/main/java/com/warlock/user/service/UserService.java
@@ -6,7 +6,6 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import javax.validation.Valid;
 import java.util.Date;
 
 @Service
