c62d71a712fd6f3a06d0a1c43d8032e1b4eb6779
Roger Worrell <raworre@gmail.com>
2023-04-12 16:51:55 -0600

    cleaning up imports

--- apps/user-manager/src/test/java/com/warlock/user/controller/UserControllerRegisterTest.java
+++ apps/user-manager/src/test/java/com/warlock/user/controller/UserControllerRegisterTest.java
@@ -2,30 +2,21 @@ package com.warlock.user.controller;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.warlock.user.model.LoginRequest;
 import com.warlock.user.model.RegistrationRequest;
 import com.warlock.user.service.UserService;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verifyNoInteractions;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
